5db4e2095c2cbae0055458ad612b590571dfc480
Chairozy <chairozyrozy@gmail.com>
2021-01-19 11:17:06 +0700

    membuat database

--- User-Management/database/migrations/2021_01_19_033340_create_table_roles.php
+++ User-Management/database/migrations/2021_01_19_033340_create_table_roles.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTableRoles extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('table_roles', function (Blueprint $table) {
             $table->id();
             $table->integer('role_id');
             $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->datetime('created_at');
-            $table->rememberToken();
             $table->datetime('updated_at');
             $table->timestamps();
         });
@@ -35,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('table_roles');
     }
 }
